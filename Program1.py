930f72cdc50b672d1fe8afea4691647c4bf214f6
rohankhatik <rc7354463@gmail.com>
2026-01-07 17:38:03 +0530

    Added first python progran

---
+++ Program1.py
@@ -0,0 +1,8 @@
+a = int(input("Please enter first number"))
+b = int(input("Please enter second number"))
+
+print ("The calculation of", a, "+" ,b, "is", a+b )
+print ("The calculation of", a, "-" ,b, "is", a-b )
+print ("The calculation of", a, "/" ,b, "is", a/b )
+print ("The calculation of", a, "*" ,b, "is", a*b )
+print ("The calculation of", a, "%" ,b, "is", a%b )
